91f9d0f4429016512fc08885314dd7aa6cb70f6f
martinromario55 <martinromario55@gmail.com>
2024-07-28 09:40:46 +0300

    Meditation Details Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,38 @@
-import { Slot, Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import { Slot, SplashScreen, Stack } from 'expo-router'
+import { useEffect } from 'react'
+
+// This will prevent the splash screen from auto hiding until loading all the font assets.
+SplashScreen.preventAutoHideAsync()
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    'Roboto-Mono': require('../assets/fonts/RobotoMono-Regular.ttf'),
+  })
+
+  useEffect(() => {
+    if (error) {
+      throw error
+    }
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  if (!fontsLoaded && !error) {
+    return null
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
     </Stack>
   )
 }
